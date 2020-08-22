be3c75f01cb816db4f75d8757a275f3de98fd54e
Geoff Grossman <geoff.grossman.dev@gmail.com>
2020-08-22 15:01:21 -0400

    it's auto drawing a box on my face

--- pixelatedFace.js
+++ pixelatedFace.js
@@ -1,6 +1,7 @@
 const video = document.querySelector('.webcam');
 const canvas = document.querySelector('.video');
 const ctx = canvas.getContext('2d');
+
 const faceCanvas = document.querySelector('.face');
 const Facectx = canvas.getContext('2d');
 
@@ -16,7 +17,30 @@ async function populateVideo() {
         video: {width: 1280, height: 720}
     });
   video.srcObject = stream;
-  await video.play()
+  await video.play();
+//   size the canvases to be the same size as the video
+console.log(video.videoWidth, video.videoHeight)
+canvas.width = video.videoWidth;
+canvas.height = video.videoHeight;
+faceCanvas.width = video.videoWidth;
+faceCanvas.height = video.videoHeight;
 }
 
-populateVideo()
+async function detect() {
+    const faces = await faceDetector.detect(video);
+    console.log(faces.length);
+    //ask the browser when the next animation fram is and tell it to run detect for us
+    faces.forEach(drawFace)
+    requestAnimationFrame(detect);
+}
+
+
+function drawFace(face) {
+    const { width, height, top, left} = face.boundingBox;
+    ctx.strokeStyle = '#ffc600';
+    ctx.lineWidth = 2;
+    ctx.strokeRect(left, top, width, height);
+}
+
+
+populateVideo().then(detect);
