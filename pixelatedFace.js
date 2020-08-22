49ea1fb9a51c04a8226fa77a23ff91f4811c9951
Geoff Grossman <geoff.grossman.dev@gmail.com>
2020-08-22 15:34:00 -0400

    MVP completed!

--- pixelatedFace.js
+++ pixelatedFace.js
@@ -3,11 +3,10 @@ const canvas = document.querySelector('.video');
 const ctx = canvas.getContext('2d');
 
 const faceCanvas = document.querySelector('.face');
-const Facectx = canvas.getContext('2d');
-
-//make a new face detector
-
+const faceCtx = faceCanvas.getContext('2d');
 const faceDetector = new window.FaceDetector();
+const SIZE = 10;
+const SCALE = 1.35;
 
 
 //write a function that will populate the users video
@@ -31,16 +30,49 @@ async function detect() {
     console.log(faces.length);
     //ask the browser when the next animation fram is and tell it to run detect for us
     faces.forEach(drawFace)
+    faces.forEach(censor)
     requestAnimationFrame(detect);
 }
 
 
 function drawFace(face) {
     const { width, height, top, left} = face.boundingBox;
+    ctx.clearRect(0,0, canvas.width, canvas.height)
     ctx.strokeStyle = '#ffc600';
     ctx.lineWidth = 2;
     ctx.strokeRect(left, top, width, height);
 }
 
+function censor({boundingBox: face}){
+    faceCtx.imageSmoothingEnabled = false;
+    faceCtx.clearRect(0,0,faceCanvas.width,faceCanvas.height);
+    // draw the small face
+    faceCtx.drawImage(
+video, 
+face.x, 
+face.y, 
+face.width,
+face.height,
+face.x,
+face.y,
+SIZE,
+SIZE
+    );
+
+    const width = face.width * SCALE;
+    const height = face.height * SCALE;
+    // take that face back out and draw it back at normal size
+    faceCtx.drawImage(
+        faceCanvas,
+        face.x, // where does the source pull from
+        face.y ,
+        SIZE,
+        SIZE,
+        face.x - (width - face.width) / 2,
+        face.y- (height - face.height) / 2,
+        width,
+        height
+    )
+}
 
 populateVideo().then(detect);
